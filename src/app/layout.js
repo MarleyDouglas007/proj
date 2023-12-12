10789d56fcd6e40dbbb934d52e9628288271ccd6
Shara5904 <128625731+Shara5904@users.noreply.github.com>
2023-12-12 11:26:48 -0300

    ATU.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,51 @@
 import './globals.css'
 import styles from './styles.module.css'
 import Image from 'next/image'
-import logo from  '../../public/Frame 7.png'
+import logo from '../../public/Frame 7.png'
 import premium from '../../public/Frame 8.png'
 import search from '../../public/Rectangle 4.png'
 import icon from '../../public/Account.png'
 
-function InicialL(){
-  return(
+function InicialL() {
+  return (
     <span className={styles.degrad}></span>
   );
 }
-export const metadata ={
+export const metadata = {
   title: 'Freebie | Planty |  Plants E-commerce Landing Page Design',
 }
 
-function Logo ({x, y}){
-  return(
-    <div style={{left:x, top:y}} className={styles.logo}>
-      <Image src={logo}/>
+function Logo({ x, y }) {
+  return (
+    <div style={{ left: x, top: y }} className={styles.logo}>
+      <Image src={logo} />
     </div>
   )
 }
-function ItemMenu ({nome}){
-  return(
-
-    <p className={styles.itemMenu}>{nome}</p>
+function ItemMenu({ primeiraLetra, textoRestante, estiloPrimeiraLetra }) {
+  return (
+    <div>
+      <p className={styles.itemMenu}>
+        <span style={estiloPrimeiraLetra}>{primeiraLetra}</span>{textoRestante}
+      </p>
+    </div>
   );
-
 }
+// function ItemMenu({ nome, primeiraLetra, textoRestante, estiloPrimeiraLetra }) {
+//   return (
+//     <div>
+//       {/* <p className={styles.itemMenu}>{nome}</p> */}
+//       <p className={styles.itemMenu}><span style={estiloPrimeiraLetra}>{primeiraLetra}</span>{textoRestante}</p>
 
-function Menu({children}){
-  return(
+//     </div>
+//   );
+
+// }
+
+function Menu({ children }) {
+  return (
     <ul className={styles.menu}>
-      {children.map((itemMenu)=>(
+      {children.map((itemMenu) => (
         <li key={itemMenu.nome}>{itemMenu}</li>
       ))}
     </ul>
@@ -42,48 +54,65 @@ function Menu({children}){
 
 
 //aaa
-function Premium () {
-  return(
+function Premium() {
+  return (
     <p className={styles.premium}>
-      <Image src={premium}/>
+      <Image src={premium} />
     </p>
   );
 }
 
-function Search () {
-  return(
+function Search() {
+  return (
     <p className={styles.search}>
       <span className={styles.s}>S</span>earch <span className={styles.anime}>A</span>nime
     </p>
   );
 }
 
-function Icon () {
-  return(
+function Icon() {
+  return (
     <p className={styles.icon}>
-      <Image src={icon}/>
+      <Image src={icon} />
     </p>
   );
 }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
           <div className={styles.nav}>
-
-          
-          <Logo x={70} y={58}/>
-          <Menu>
-            <ItemMenu nome = <p className={styles.degrad}>Geren</p>/>
-             <ItemMenu nome = "Movies"/>
-             <ItemMenu nome = "ONAs"/>
-             <ItemMenu nome = "News"/>
-          </Menu>
-          <Premium />
-          <Search/>
-          <Icon />
+            <Logo x={70} y={58} />
+            <Menu>
+              <ItemMenu
+                primeiraLetra="G"
+                textoRestante="enre"
+                estiloPrimeiraLetra={{
+                  background: 'linear-gradient(114deg, #C93677 -2.94%, #33ADDF 41.89%)',
+                  WebkitBackgroundClip: 'text',
+                  WebkitTextFillColor: 'transparent',
+                }}
+              />
+              <ItemMenu
+                primeiraLetra="M"
+                textoRestante="ovies"
+                estiloPrimeiraLetra={{ color: 'blue' }}
+              />
+              <ItemMenu
+                primeiraLetra="O"
+                textoRestante="NAs"
+                estiloPrimeiraLetra={{ color: 'blue' }}
+              />
+              <ItemMenu
+                primeiraLetra="N"
+                textoRestante="ews"
+                estiloPrimeiraLetra={{ color: 'blue' }}
+              />
+            </Menu>
+            <Premium />
+            <Search />
+            <Icon />
           </div>
         </header>
         <main>
@@ -93,3 +122,31 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <header>
+//           <div className={styles.nav}>
+
+
+//             <Logo x={70} y={58} />
+//             <Menu>
+//               {/* <ItemMenu nome=<p className={styles.degrad}>Geren</p> /> */}
+//               <ItemMenu primeiraLetra="G" textoRestante="enre" estiloPrimeiraLetra={{ color: 'blue' }} />
+//               <ItemMenu primeiraLetra="M" textoRestante="ovies" estiloPrimeiraLetra={{ color: 'blue' }} />
+//               <ItemMenu primeiraLetra="O" textoRestante="NAs" estiloPrimeiraLetra={{ color: 'blue' }} />
+//               <ItemMenu primeiraLetra="N" textoRestante="ews" estiloPrimeiraLetra={{ color: 'blue' }} />
+//             </Menu>
+//             <Premium />
+//             <Search />
+//             <Icon />
+//           </div>
+//         </header>
+//         <main>
+//           {children}
+//         </main>
+//       </body>
+//     </html>
+//   )
+// }
